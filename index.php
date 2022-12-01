0564f7965702526aefaf0514d5b5c6d6622aa023
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2022-12-01 18:17:14 +0100

    basic authentication

--- index.php
+++ index.php
@@ -5,8 +5,11 @@ require 'Routing.php';
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
+Routing::get('clicker', 'DefaultController');
 Routing::get('login', 'DefaultController');
 Routing::get('register', 'DefaultController');
-Routing::get('clicker', 'DefaultController');
+
+Routing::post('loginUser', 'SecurityController');
+Routing::post('registerUser', 'SecurityController');
 
 Routing::Run($path);
