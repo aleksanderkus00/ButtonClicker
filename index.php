d8b9fe54103abf63bb4531c22f919757fc101586
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2022-12-01 17:18:39 +0100

    add simple routing

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('login', 'DefaultController');
+Routing::get('register', 'DefaultController');
+Routing::get('clicker', 'DefaultController');
+
+Routing::Run($path);
