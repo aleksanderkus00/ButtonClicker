8e52a4bedef14a99afe08757e3d9cd645fcf7ec4
Aleksander Kuś <91941766+aleksanderkus00@users.noreply.github.com>
2023-01-09 21:14:16 +0100

    Refactor

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,3 @@
-const PROFILEID = "profile";
-const RANKINGID = "rankingTable";
-const SETTINGSID = "settings";
-const PROFILEBUTTONID = "profileButton";
-const RANKINGBUTTONID = "rankingButton";
-const SETTINGSBUTTONID = "settingsButton";
-
 let userClicks = document.querySelector("#userClicks");
 let nickname = document.querySelector("#nickname");
 let email = document.querySelector("#email");
@@ -22,64 +15,7 @@ window.addEventListener("load", (event) => {
   email.value = cookieEmail.replace("%", "@");
 });
 
-function showTab(descId, sorceId) {
-  let profile = document.getElementById(PROFILEID);
-  let ranking = document.getElementById(RANKINGID);
-  let settings = document.getElementById(SETTINGSID);
-  let profileButton = document.getElementById(PROFILEBUTTONID);
-  let rankingButton = document.getElementById(RANKINGBUTTONID);
-  let settingsButton = document.getElementById(SETTINGSBUTTONID);
-
-  // TODO: add disable button border bottom if clicked
-  if (descId === PROFILEID) {
-    displayProfile(profile, ranking, settings, profileButton);
-  } else if (descId === RANKINGID) {
-    displayRanking(profile, ranking, settings, rankingButton);
-  } else if (descId === SETTINGSID) {
-    displaySettings(profile, ranking, settings, settingsButton);
-  }
-}
-
-function displayProfile(profile, ranking, settings) {
-  makeVisible(profile);
-  makeInvisible(ranking);
-  makeInvisible(settings);
-}
-
-function displayRanking(profile, ranking, settings) {
-  makeInvisible(profile);
-  makeVisible(ranking);
-  makeInvisible(settings);
-}
-
-function displaySettings(profile, ranking, settings) {
-  makeInvisible(profile);
-  makeInvisible(ranking);
-  makeVisible(settings);
-}
-
-function makeInvisible(tab) {
-  tab.style.display = "none";
-}
-
-function makeVisible(tab) {
-  tab.style.display = "block";
-}
-
-function disableButtonBorderBottom(button) {
-  button.style.borderBottom = "0px";
-}
-function AbleButtonBorderBottom(button) {
-  button.style.borderBottom = "1px solid #000000";
-}
-
-/*
-    tab.style.borderBottom = '0px';
-    tab.style.borderBottom = '1px solid #000000';
-*/
-
 function buttonClick() {
-  //let number = Number(getCookie("clicks"));
   clicks += 1;
   counter += 1;
   userClicks.innerHTML = clicks;
---
+++ public/js/tabSwitcher.js
@@ -0,0 +1,61 @@
+const PROFILEID = "profile";
+const RANKINGID = "rankingTable";
+const SETTINGSID = "settings";
+const PROFILEBUTTONID = "profileButton";
+const RANKINGBUTTONID = "rankingButton";
+const SETTINGSBUTTONID = "settingsButton";
+
+function showTab(descId, sorceId) {
+  let profile = document.getElementById(PROFILEID);
+  let ranking = document.getElementById(RANKINGID);
+  let settings = document.getElementById(SETTINGSID);
+  let profileButton = document.getElementById(PROFILEBUTTONID);
+  let rankingButton = document.getElementById(RANKINGBUTTONID);
+  let settingsButton = document.getElementById(SETTINGSBUTTONID);
+
+  if (descId === PROFILEID) {
+    displayProfile(profile, ranking, settings, profileButton);
+  } else if (descId === RANKINGID) {
+    displayRanking(profile, ranking, settings, rankingButton);
+  } else if (descId === SETTINGSID) {
+    displaySettings(profile, ranking, settings, settingsButton);
+  }
+}
+
+function displayProfile(profile, ranking, settings) {
+  makeVisible(profile);
+  makeInvisible(ranking);
+  makeInvisible(settings);
+}
+
+function displayRanking(profile, ranking, settings) {
+  makeInvisible(profile);
+  makeVisible(ranking);
+  makeInvisible(settings);
+}
+
+function displaySettings(profile, ranking, settings) {
+  makeInvisible(profile);
+  makeInvisible(ranking);
+  makeVisible(settings);
+}
+
+function makeInvisible(tab) {
+  tab.style.display = "none";
+}
+
+function makeVisible(tab) {
+  tab.style.display = "block";
+}
+
+function disableButtonBorderBottom(button) {
+  button.style.borderBottom = "0px";
+}
+function AbleButtonBorderBottom(button) {
+  button.style.borderBottom = "1px solid #000000";
+}
+
+/*
+    tab.style.borderBottom = '0px';
+    tab.style.borderBottom = '1px solid #000000';
+*/
